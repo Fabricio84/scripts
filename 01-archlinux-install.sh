1c56613fd2f72716e986ea410bd0315bcc936eb5
Fabricio Souza <fabricio.abner@gmail.com>
2020-09-01 12:37:35 -0300

    first commit

---
+++ 01-archlinux-install.sh
@@ -0,0 +1,121 @@
+#!bin/bash
+is_connected () {
+   if [ "`ping -c 1 www.google.com.br`" ]
+    then
+      return 0
+    else
+      return 1
+   fi
+}
+
+set_locale () {
+  echo "Setting locale to pt_BR.UTF-8"
+  sed -i 's/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g' /etc/locale.gen
+  locale-gen
+  export LANG=pt_BR.UTF-8  
+
+  echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+  echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
+}
+
+set_datetime () {
+  echo "Setting date time"
+  timedatectl set-ntp true
+}
+
+create_partition_efi () {
+  echo "Setting disk partition"
+
+  echo -e "\tcreating EFI partition"
+  sudo parted -s /dev/sda mklabel gpt mkpart ESP fat32 1MiB 261MiB set 1 esp on
+
+  echo -e "\tcreating root partition"
+  sudo parted -s /dev/sda mkpart primary ext4 261MiB 20GiB
+
+  echo -e "\tcreating swap partition"
+  sudo parted -s /dev/sda mkpart primary linux-swap 20GiB 24GiB
+
+  echo -e "\tcreating home partition"
+  sudo parted -s /dev/sda mkpart primary ext4 24GiB 100%
+}
+
+format_partitions () {
+  echo "Formating partitions"
+  sudo mkfs.vfat -F 32 /dev/sda1
+  sudo mkfs.ext4 -F /dev/sda2
+  sudo mkfs.ext4 -F /dev/sda4
+}
+
+enable_swap_partition () {
+  echo "Enable swap partition"
+  sudo mkswap /dev/sda3
+  sudo swapon /dev/sda3
+}
+
+mount_partitions () {
+  echo "Mounting root partition"
+  mount /dev/sda2 /mnt
+  mkdir /mnt/boot
+  mount /dev/sda1 /mnt/boot
+}
+
+linux_install () {
+  echo "Install essentials packages"
+  pacman -Syy
+  pacman -S reflector --noconfirm
+  cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+  reflector -c "BR" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+  pacstrap /mnt base linux linux-firmware
+}
+
+fstab_generate () {
+  echo "Fstab generate..."
+  genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+arch_chroot () {
+  echo "Changing root"
+  cp 02-archlinux-install.sh /mnt
+  arch-chroot /mnt bash 02-archlinux-install.sh $root_password $username $password
+}
+
+wifi_connect () {
+  echo "Setting network wifi"
+  pacman -S networkmanager
+  printf "Typing your SSID: "
+  read -r ssid
+  printf "Typing your password: "
+  read -r passw
+  nmcli device wifi connect $ssid password $passw
+}
+
+read_credentials () {
+  printf "Digite o nome para usuário: "
+  read username
+
+  printf "Digite a senha para $username: "
+  read password
+
+  printf "Digite a senha para o root: "
+  read root_password
+}
+
+main () {
+  # MAIN
+  echo "ArchLinux 64 installing..."
+
+  read_credentials
+
+  set_locale
+  set_datetime
+  create_partition_efi
+  format_partitions
+  enable_swap_partition
+  mount_partitions
+  linux_install
+  fstab_generate
+  arch_chroot
+  reboot now
+}
+
+main
---
+++ 02-archlinux-install.sh
@@ -0,0 +1,152 @@
+#bin/bash
+keyboard_layout_setting_br () {
+  echo "Setting keyboard layout to br-abnt2"
+  loadkeys br-abnt2
+  setxkbmap -model abnt2 -layout br
+}
+
+set_locale () {
+  echo "Setting locale to pt_BR.UTF-8"
+  sed -i 's/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g' /etc/locale.gen
+  locale-gen
+  export LANG=pt_BR.UTF-8  
+
+  echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+  echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
+}
+
+set_datetime () {
+  echo "Setting date time"
+  timedatectl set-ntp true
+}
+
+set_timezone () {
+  echo "Setting timezone to America/Sao_Paulo"
+  ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+  hwclock --systohc
+}
+
+set_hosts () {
+  echo "Setting hosts"
+  echo "archlinux" > /etc/hostname
+
+  echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+  echo "::1		localhost.localdomain	localhost" >> /etc/hosts
+  echo "127.0.1.1	archlinux.localdomain	archlinux" >> /etc/hosts
+}
+
+mkinitcpio_set_hooks_keymap () {
+  echo "Setting mkinitcpio.conf add HOOK keymap"
+  sed -i 's/fsck)/fsck keymap)/g' /etc/mkinitcpio.conf
+  mkinitcpio -P
+}
+
+sudo_install_user_add () {
+  echo "Configure user"
+
+  root_password=$1
+  username=$2
+  password=$3
+
+  useradd -m $username
+
+  pacman -S sudo --noconfirm
+  echo "$username ALL=(ALL:ALL) ALL" >> /etc/sudoers
+  visudo -c
+
+  echo -e "$password\n$password" | (passwd $username)
+  echo -e "$root_password\n$root_password" | (passwd root)
+}
+
+systemd_boot_uefi_intel_ucode () {
+  echo "Setting boot UEFI with intel-ucode"
+
+  bootctl install
+  pacman -S intel-ucode --noconfirm
+
+  echo "default  arch" > /boot/loader/loader.conf
+  echo "timeout 4" >> /boot/loader/loader.conf
+
+  rootPARTUUID=$(blkid /dev/sda2 -s PARTUUID -o value)
+  echo "title Arch Linux" > /boot/loader/entries/arch.conf
+  echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+  echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
+  echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+  echo "options root=PARTUUID=$rootPARTUUID rw" >> /boot/loader/entries/arch.conf
+}
+
+desktopenviroment_bugdie_install () {
+  sudo pacman -S xorg-server xterm xorg-xinit xf86-video-intel --noconfirm
+  sudo pacman -S budgie-desktop budgie-extras --noconfirm
+}
+
+term_alacritty_install_settings () {
+  pacman -S alacritty --noconfirm
+}
+
+displaymanager_gdm_install () {
+  pacman -S gdm --noconfirm
+}
+
+networkmanager_install () {
+    pacman -S networkmanager --noconfirm
+    systemctl enable networkManager.service
+    systemctl restart networkManager.service
+}
+
+install_apps () {
+  pacman -S pantheon-files firefox 
+}
+
+install_apps_dev () {
+  pacman -S code vim tmux nano alacritty git curl docker --noconfirm
+
+  #git configure
+  git config --global user.name "Fabricio Souza"
+  git config --global user.email "fabricio.abner@gmail.com"
+
+  #docker configure
+  sudo usermod -aG docker $USER
+
+  # install asdf
+  # to update asdf with (asdf update)
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+  echo -e "\n. $HOME/.asdf/asdf.sh" >> ~/.bashrc
+  echo -e "\n. $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
+
+  # install asdf-nodejs
+  # to update plugins with (asdf plugin-update --all)
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+
+  # install nodejs 12.18.2 by asdf
+  asdf install nodejs 12.18.2
+  asdf global nodejs 12.18.2
+  # inside folder can i?
+  #asdf local nodejs [other version]
+}
+
+create_ssh_key () {
+  echo "SSH-KEY generate..."
+  echo -e "$passphrase\n$passphrase" | (ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C $key_comment)
+
+  echo -e "\t add SSH-KEY to ssh-agent"
+  echo -e "$passphrase" | (ssh-add ~/.ssh/id_ed25519)
+}
+
+main () {
+  set_timezone
+  set_locale
+  set_hosts
+  keyboard_layout_setting_br
+  mkinitcpio_set_hooks_keymap
+  systemd_boot_uefi_intel_ucode
+  networkmanager_install
+  desktopenviroment_bugdie_install
+  displaymanager_gdm_install
+  term_alacritty_install_settings
+  sudo_install_user_add $1 $2 $3
+}
+
+main $1 $2 $3
+
